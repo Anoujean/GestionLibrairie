de59638f0942bc6acb60d4960f5c5ef11bbd06b6
AJB <anoujeanbalachandran@gmail.com>
2020-02-03 13:36:30 +0100

    avec des changement

--- gestion_des_dates.java
+++ gestion_des_dates.java
@@ -4,3 +4,5 @@ Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
 // date -> String
 DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");  
 String strDate = dateFormat.format(date1);   
+
+// à tester
