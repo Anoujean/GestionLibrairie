a8bb862db3c810aa649bbcc6a111c4beb4a37ad4
AJB <anoujeanbalachandran@gmail.com>
2020-02-03 13:23:48 +0100

    gestion_des_dates added

---
+++ gestion_des_dates.java
@@ -0,0 +1,6 @@
+//string -> date
+String sDate1="31/12/1998";  
+Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);  
+// date -> String
+DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");  
+String strDate = dateFormat.format(date1);   
